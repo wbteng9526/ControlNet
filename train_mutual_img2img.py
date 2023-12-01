b23fb69fa8487095cc0cfc448ab5adcb01c78730
Wenbin Teng <wteng@smtp.ict.usc.edu>
2023-11-30 21:43:14 -0800

    gpu

--- train_mutual_img2img.py
+++ train_mutual_img2img.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     dataset = MyMutualDataset()
     dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
     logger = ImageLogger(batch_frequency=logger_freq)
-    trainer = pl.Trainer(gpus=2, precision=32, callbacks=[logger])
+    trainer = pl.Trainer(gpus=1, precision=32, callbacks=[logger])
 
 
     # Train!
